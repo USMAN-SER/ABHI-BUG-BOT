38c0e8a5ae40493898368548deab14675c74c392
USMAN-SER <141281506+USMAN-SER@users.noreply.github.com>
2024-09-23 10:56:56 +0500

    Update settings.js

--- settings.js
+++ settings.js
@@ -2,26 +2,26 @@ const fs = require('fs')
 const chalk = require('chalk')
 
 //contact details
-global.ownernomer = "919074692450"
-global.ownername = "Abhishek Suresh"
-global.ytname = "YT: Comedy Melody CH"
-global.socialm = "GitHub: DGXeon"
-global.location = "India, Kerala, Idukki"
+global.ownernomer = "+923351300389"
+global.ownername = "USMAN-SER"
+global.ytname = "YT: USMAN-SER
+global.socialm = "GitHub: USMAN-SER"
+global.location = "Pak, Layyah, Hera Adda"
 
-global.ownernumber = '919074692450'  //creator number
-global.ownername = 'Abhishek Suresh' //owner name
-global.botname = '𝙰𝙱𝙷𝙸-𝙱𝚄𝙶-𝙱𝙾𝚃' //name of the bot
+global.ownernumber = '923351300389'  //creator number
+global.ownername = 'USMAN-SER' //owner name
+global.botname = 'U S M A N' //name of the bot
 
 //sticker details
-global.packname = '🎯𝙰𝙱𝙷𝙸-𝙱𝚄𝙶-𝙱𝙾𝚃'
-global.author = '𝙰𝙱𝙷𝙸𝚂𝙷𝙴𝙺 𝚂𝚄𝚁𝙴𝚂𝙷🍀'
+global.packname = '🎯USMAN-SER
+global.author = 'U S M A N'
 
 //console view/theme
 global.themeemoji = '🎯'
-global.wm = "©𝙰𝙱𝙷𝙸-𝙱𝚄𝙶-𝙱𝙾𝚃"
+global.wm = "©U S M A N
 
 //theme link
-global.link = 'https://chat.whatsapp.com/BOLb0ICN3sAJ5dloRBw5VD'
+global.link = 'https://chat.whatsapp.com/+923351300389'
 
 //custom prefix
 global.prefa = ['','!','.','#','&']
@@ -29,11 +29,11 @@ global.prefa = ['','!','.','#','&']
 //false=disable and true=enable
 global.autoRecording = false //auto recording
 global.autoTyping = false //auto typing
-global.autorecordtype = false //auto typing + recording
+global.autorecordtype = true //auto typing + recording
 global.autoread = false //auto read messages
 global.autobio = false //auto update bio
 global.anti92 = false //auto block +92 
-global.autoread_status = false //auto view status/story
+global.autoread_status = true //auto view status/story
 
 //menu type 
 //v1 is image menu, 
